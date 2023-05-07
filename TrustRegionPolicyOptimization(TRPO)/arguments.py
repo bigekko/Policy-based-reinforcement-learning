72d0582e9030727d4e254a233d251a93c0930b32
Xu Zhang <92803389+bigekko@users.noreply.github.com>
2023-05-07 21:03:59 +0800

    Add files via upload

---
+++ TrustRegionPolicyOptimization(TRPO)/arguments.py
@@ -0,0 +1,16 @@
+class ARGS:
+    def __init__(self) -> None:
+        self.gamma = 0.99
+        self.env_name = "LunarLanderContinuous-v2"
+        self.seed = 123
+        self.save_dir = "saved_models/"
+        self.total_timesteps = 1000000
+        self.nsteps = 1024
+        self.lr = 3e-4
+        self.batch_size = 64
+        self.vf_itrs = 5
+        self.tau = 0.95
+        self.damping = 0.1
+        self.max_kl = 0.01
+        self.cuda = True
+        self.log_dir = "logs"
